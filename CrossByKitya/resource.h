e029ee66bf31a999a7760c3283ec8102f2f368f1
Ivan <43240272+pinkyhi@users.noreply.github.com>
2020-05-24 17:23:09 +0300

    Revert "TRY"
    This reverts commit 10647510b4c18d6274db1ec4719754a8105471b5.

--- CrossByKitya/resource.h
+++ CrossByKitya/resource.h
@@ -2,20 +2,20 @@
 // Microsoft Visual C++ generated include file.
 // Used by CrossByKitya.rc
 
-#define IDC_MYICON                      2
-#define IDD_CROSSBYKITYA_DIALOG         102
-#define IDS_APP_TITLE                   103
-#define IDD_ABOUTBOX                    103
-#define IDM_ABOUT                       104
-#define IDM_EXIT                        105
-#define IDI_CROSSBYKITYA                107
-#define IDI_SMALL                       108
-#define IDC_CROSSBYKITYA                109
-#define IDR_MAINFRAME                   128
-#define IDD_EXIT                        130
-#define AcceptExit                      1000
-#define RegretExit                      1001
-#define IDC_STATIC                      -1
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_CROSSBYKITYA_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_CROSSBYKITYA			107
+#define IDI_SMALL				108
+#define IDC_CROSSBYKITYA			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
 // Next default values for new objects
 //
 #ifdef APSTUDIO_INVOKED
